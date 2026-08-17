304a97dea9713672c8d333dcf8d5947bcaac0cb1
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2026-08-16 17:40:17 -0700

    add methods for rect

--- src/main/java/brachy/modularui/api/layout/IViewportStack.java
+++ src/main/java/brachy/modularui/api/layout/IViewportStack.java
@@ -1,6 +1,7 @@
 package brachy.modularui.api.layout;
 
 import brachy.modularui.screen.viewport.TransformationMatrix;
+import brachy.modularui.utils.Rectangle;
 import brachy.modularui.widget.sizer.Area;
 
 import com.mojang.blaze3d.vertex.PoseStack;
@@ -147,6 +148,19 @@ public interface IViewportStack {
     int transformY(float x, float y);
 
     /**
+     * Transforms the x and y components of an area's position with the current matrix transformations.
+     *
+     * @param rect the area to transform
+     * @return copy of rect with transformations applied
+     */
+    default Rectangle transformRect(Rectangle rect) {
+        Rectangle ret = new Rectangle(rect);
+        ret.x(transformX(rect.x, rect.y));
+        ret.y(transformY(rect.x, rect.y));
+        return ret;
+    }
+
+    /**
      * Transforms the x component of a position with the current inverted matrix transformations.
      *
      * @param x x component of position
@@ -163,6 +177,19 @@ public interface IViewportStack {
      * @return un-transformed y component
      */
     int unTransformY(float x, float y);
+
+    /**
+     * Transforms the x and y components of an area's position with the current inverted matrix transformations.
+     *
+     * @param rect the area to transform
+     * @return copy of area with transformations applied
+     */
+    default Rectangle unTransformRect(Rectangle rect) {
+        Rectangle ret = new Rectangle(rect);
+        ret.x(unTransformX(rect.x, rect.y));
+        ret.y(unTransformY(rect.x, rect.y));
+        return ret;
+    }
 
     /**
      * Transforms a vector with the current matrix transformations.
